2745ae17918e52f13ac6a17dfffd012ac74ae2ed
Bruna Rocha <bsant4131@gmail.com>
2024-12-19 14:39:37 -0300

    Melhora as instruções iniciais, e faz um sistema de feedback a cadachute do usuário.

--- guess_number.py
+++ guess_number.py
@@ -1,8 +1,15 @@
 import random 
 
+print("Bem-vindos ao jogo!")
+print("Tente advinhar o número que estou pensando!")
+
+print("Quero que você me dê um número como o máximo que posso pensar, 0 acima!")
+
+print("Se você escolher 100, eu estarei pensando em um número entre 1 e 100.")
+
 top_of_range = input("Escreva um número: ")
 
-if top_of_range.isdigit():
+if top_of_range.lstrip('-').isdigit():
     top_of_range = int(top_of_range)
 
     if top_of_range <= 0:
@@ -12,6 +19,29 @@ else:
     print("Por favor, digite um número da próxima vez.")
     quit()
 
-
+# Gera o número aleatório entre 1 e o número informado
 random_number = random.randint(1, top_of_range)
-print(random_number)
+
+guesses = 0
+
+# Loop de adivinhação
+while True:
+    guesses += 1
+    user_guess = input("Faça um chute: ")
+
+    # Verifica se o valor informado é um número inteiro
+    if user_guess.lstrip('-').isdigit():
+        user_guess = int(user_guess)
+    else:
+        print("Por favor, digite um número da próxima vez.")
+        continue
+
+    # Checa se o chute do usuário é igual ao número gerado
+    if user_guess == random_number:
+        print('Você acertou!')
+        print("Você acertou em", guesses, "tentativas.")
+        quit()  # Sai do programa após o acerto
+    elif user_guess > random_number:
+        print("Você chutou muito alto!")
+    else:
+        print("Você chutou muito baixo!")
